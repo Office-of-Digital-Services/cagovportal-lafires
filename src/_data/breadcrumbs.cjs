4c717604344fffd0595dd08c03982f32b0778738
kkoryaka <konstantin.koryaka@state.ca.gov>
2025-03-04 15:30:35 -0800

    initial commit

---
+++ src/_data/breadcrumbs.cjs
@@ -0,0 +1,142 @@
+//@ts-check
+
+/** @type {breadcrumbnode[]} */
+const data = [
+  {
+    key: "Home",
+    href: "https://www.ca.gov",
+    children: [
+      {
+        key: "Services",
+        href: "https://www.ca.gov/services/",
+        children: [
+          {
+            key: "Topics",
+            href: "https://www.ca.gov/topics/",
+          },
+        ],
+      },
+      {
+        key: "Departments",
+        href: "https://www.ca.gov/departments/",
+      },
+      {
+        key: "2025 Los Angeles Fires",
+        href: "/",
+        children: [
+          {
+            key: "Get help in person",
+            href: "/get-help-in-person/",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+/**
+ * @typedef {object} breadcrumbnode
+ * @property {string} key
+ * @property {string} href
+ * @property {breadcrumbnode[]} [children]
+ */
+
+/**
+ * @typedef {object} flatbreadcrumb
+ * @property {string} key
+ * @property {breadcrumblink[]} links
+ */
+
+/**
+ * @typedef {object} breadcrumblink
+ * @property {string} title
+ * @property {string} href
+ */
+
+function flattenData() {
+  /** @type {flatbreadcrumb[]} */
+  const result = [];
+
+  /**
+   * @param {breadcrumbnode} node
+   * @param {breadcrumblink[]} [links]
+   */
+  function flattenRecursive(node, links = []) {
+    /** @type {flatbreadcrumb} */
+    const flatbreadcrumb = {
+      key: node.key,
+      links: [...links, { title: node.key, href: node.href }],
+    };
+
+    result.push(flatbreadcrumb);
+
+    node.children?.forEach((child) =>
+      flattenRecursive(child, flatbreadcrumb.links)
+    );
+  }
+
+  data.forEach((x) => flattenRecursive(x));
+
+  return result;
+}
+
+module.exports = flattenData();
+
+/*
+Final data looks like this
+
+const data = [
+  {
+    key: "Home",
+    links: [
+      {
+        title: "Home",
+        href: "/"
+      }
+    ]
+  },
+  {
+    key: "Services",
+    links: [
+      {
+        title: "Home",
+        href: "/"
+      },
+      {
+        title: "Services",
+        href: "/services/"
+      }
+    ]
+  },
+  {
+    key: "Departments",
+    links: [
+      {
+        title: "Home",
+        href: "/"
+      },
+      {
+        title: "Departments",
+        href: "/departments/"
+      }
+    ]
+  },
+  {
+    key: "Topics",
+    links: [
+      {
+        title: "Home",
+        href: "/"
+      },
+      {
+        title: "Services",
+        href: "/services/"
+      },
+      {
+        title: "Topics",
+        href: "/topics/"
+      }
+    ]
+  }
+];
+*/
