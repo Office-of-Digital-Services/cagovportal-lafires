397dc6706c2cf4ef65172f6812b9a8bfda780dd6
Carter Medlin <carterm@users.noreply.github.com>
2025-06-02 11:54:12 -0700

    copy works

--- src/_includes/recovery-services-all.html.js
+++ src/_includes/recovery-services-all.html.js
@@ -1,6 +1,42 @@
 //@ts-check
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Share Plan events
+
+  const sharePlanCopy = /** @type {HTMLButtonElement | null} */ (
+    document.getElementById("share-plan-copy")
+  );
+  const sharePlanCopied = /** @type {HTMLButtonElement | null} */ (
+    document.getElementById("share-plan-copied")
+  );
+  const inputElement = /** @type {HTMLInputElement | null} */ (
+    document.getElementById("url-copy")
+  );
+  if (!inputElement || !sharePlanCopy || !sharePlanCopied) {
+    console.error("Share plan elements not found.");
+    return;
+  }
+
+  const copyFunction = () => {
+    inputElement.select();
+    console.log("Copying the URL: ", inputElement.value);
+    navigator.clipboard.writeText(inputElement.value).catch(err => {
+      console.error("Failed to copy: ", err);
+    });
+
+    sharePlanCopied.classList.remove("d-none");
+    sharePlanCopied.hidden = false;
+    sharePlanCopied.ariaHidden = null;
+    sharePlanCopied.focus();
+    sharePlanCopy.classList.add("d-none");
+    sharePlanCopy.hidden = true;
+    sharePlanCopy.ariaHidden = "true";
+  };
+
+  [sharePlanCopy, sharePlanCopied].forEach(btn =>
+    btn.addEventListener("click", copyFunction)
+  );
+
   const clearVarsButton = document.querySelector(".clearVars");
   if (clearVarsButton) {
     clearVarsButton.addEventListener("click", event => {
