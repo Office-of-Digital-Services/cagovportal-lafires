d9c6091b7d743aa923ed15cc494d0988779b31ef
Carter Medlin <carterm@users.noreply.github.com>
2025-06-03 14:32:06 -0700

    showing build errors for real translations

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -406,7 +406,9 @@ module.exports = function (
 
     // Check if the requested content key exists.
     if (!contentGroup) {
-      console.log(chalk.red(`[i18n] Could not find content group for *${key}* in translations table.`));
+      console.error(
+        `[i18n] Could not find content group for *${key}* in translations table.`
+      );
       return "";
     }
 
