91eb399304f7546baab75a652cb110951a3242d5
Carter Medlin <carterm@users.noreply.github.com>
2025-06-03 11:21:50 -0700

    mailto share works

--- src/_includes/recovery-services-finder-results.html.js
+++ src/_includes/recovery-services-finder-results.html.js
@@ -1,6 +1,21 @@
 //@ts-check
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Begin share plan functionality
+  const sharePlanModal = document.getElementById("share-plan");
+  const copyBtn = document.getElementById("share-plan-copy");
+  const copiedBtn = document.getElementById("share-plan-copied");
+  const mailBtn = /** @type {HTMLAnchorElement} */ (
+    document.getElementById("share-plan-mailto")
+  );
+  const urlInput = /** @type {HTMLInputElement | null} */ (
+    document.getElementById("url-copy")
+  );
+
+  if (!(sharePlanModal && copyBtn && copiedBtn && urlInput)) {
+    console.error("Share plan elements not found.");
+    return;
+  }
   const paramName = "selected";
 
   const allServiceDivs = [
@@ -34,6 +49,9 @@ document.addEventListener("DOMContentLoaded", () => {
         div.remove(); // Remove the div from the DOM
       }
     });
+
+    // Update the mailto link with the current URL
+    mailBtn.href = `mailto:?subject=${mailBtn.dataset.mailSubject}&body=${mailBtn.dataset.mailBody}%0A%0A${getURL().toString()}`;
   };
 
   allServiceDivs.forEach(div => {
@@ -61,43 +79,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
   hideUnselectedServices();
 
-  // Begin share plan functionality
-  const sharePlanModal = document.getElementById("share-plan");
-  const copyBtn = document.getElementById("share-plan-copy");
-  const copiedBtn = document.getElementById("share-plan-copied");
-  const urlInput = /** @type {HTMLInputElement | null} */ (
-    document.getElementById("url-copy")
-  );
+  // Bootstrap Modal Show Event
+  // Set the URL input value to the current page URL when the modal is shown
+  sharePlanModal.addEventListener("show.bs.modal", () => {
+    urlInput.value = window.location.href;
+  });
 
-  if (sharePlanModal && copyBtn && copiedBtn && urlInput) {
-    // Bootstrap Modal Show Event
-    // Set the URL input value to the current page URL when the modal is shown
-    sharePlanModal.addEventListener("show.bs.modal", () => {
-      urlInput.value = window.location.href;
-    });
+  const copyButtonClick = () => {
+    urlInput.select();
+    navigator.clipboard
+      .writeText(urlInput.value)
+      .then(() => {
+        // Show the copied button and hide the copy button
+        copiedBtn.classList.remove("d-none");
+        copiedBtn.hidden = false;
+        copiedBtn.ariaHidden = null;
+        copiedBtn.focus();
+        copyBtn.classList.add("d-none");
+        copyBtn.hidden = true;
+        copyBtn.ariaHidden = "true";
+      })
+      .catch(err => {
+        console.error("Failed to copy: ", err);
+      });
+  };
+  copyBtn.addEventListener("click", copyButtonClick);
+  copiedBtn.addEventListener("click", copyButtonClick);
 
-    const copyButtonClick = () => {
-      urlInput.select();
-      navigator.clipboard
-        .writeText(urlInput.value)
-        .then(() => {
-          // Show the copied button and hide the copy button
-          copiedBtn.classList.remove("d-none");
-          copiedBtn.hidden = false;
-          copiedBtn.ariaHidden = null;
-          copiedBtn.focus();
-          copyBtn.classList.add("d-none");
-          copyBtn.hidden = true;
-          copyBtn.ariaHidden = "true";
-        })
-        .catch(err => {
-          console.error("Failed to copy: ", err);
-        });
-    };
-    copyBtn.addEventListener("click", copyButtonClick);
-    copiedBtn.addEventListener("click", copyButtonClick);
-  } else {
-    console.error("Share plan elements not found.");
-  }
   // End share plan functionality
 });
