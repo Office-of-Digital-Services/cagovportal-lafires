4901dffc50773f636a28a62b52817c06a06300e6
kkoryaka <konstantin.koryaka@state.ca.gov>
2025-08-21 15:51:10 -0700

    updated number

--- src/_includes/track-progress.mjs
+++ src/_includes/track-progress.mjs
@@ -597,8 +597,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (malibu_rebuild_applications_received_Element) {
         const malibu_rebuild_applications_received_Value =
-          (getMetricValue("malibu_rebuild_applications_received") || 0) +
-          (getMetricValue("malibu_rebuild_applications_in_review") || 0);
+          getMetricValue("malibu_rebuild_applications_received") || 0;
         malibu_rebuild_applications_received_Element.textContent =
           malibu_rebuild_applications_received_Value !== null
             ? malibu_rebuild_applications_received_Value.toLocaleString()
